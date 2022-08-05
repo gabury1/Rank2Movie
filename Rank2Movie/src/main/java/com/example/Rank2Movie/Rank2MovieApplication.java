98e5ab37bdfb7e16313f59169fd1a68ee6efec84
DESKTOP-ANVN13Q\gabur <gabury1@gmail.com>
2022-08-05 18:20:23 +0900

    프로젝트 생성 + 프로퍼티 수정

---
+++ Rank2Movie/src/main/java/com/example/Rank2Movie/Rank2MovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.Rank2Movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rank2MovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rank2MovieApplication.class, args);
+	}
+
+}
---
+++ Rank2Movie/src/test/java/com/example/Rank2Movie/Rank2MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Rank2Movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rank2MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
